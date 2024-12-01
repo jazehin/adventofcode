839f55bfe432966f24d54f7dcadabd1dde0e96f3
Jazi <Jazi@DESKTOP-L2US8NO>
2024-12-01 20:55:36 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using adventofcode.y2024;
+using System.Reflection;
+
+namespace adventofcode
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //exit if AoC is not active (date is not between Dec 1 - Dec 25)
+            if (DateTime.Today.Month != 12 || DateTime.Now.Day > 25)
+            {
+                Console.WriteLine("Advent of Code is inactive!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            //dynamically get the class, and then the method to call
+            Type? type = null;
+            MethodInfo? methodInfo = null;
+
+            type = Type.GetType($"adventofcode.y{DateTime.Now.Year}.Day{DateTime.Now.Day:00}_1");
+            if (type is not null)
+                methodInfo = type.GetMethod("GetResult");
+            if (methodInfo is not null)
+                Console.WriteLine($"Day {DateTime.Now.Day:00}, part 1: {methodInfo.Invoke(null, null)}");
+
+            type = null;
+            methodInfo = null;
+
+            type = Type.GetType($"adventofcode.y{DateTime.Now.Year}.Day{DateTime.Now.Day:00}_2");
+            if (type is not null)
+                methodInfo = type.GetMethod("GetResult");
+            if (methodInfo is not null)
+                Console.WriteLine($"Day {DateTime.Now.Day:00}, part 2: {methodInfo.Invoke(null, null)}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ y2024/Day01_2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode.y2024
+{
+    public class Day01_2
+    {
+        public static int GetResult()
+        {
+            string input = File.ReadAllText("./y2024/res/01.txt");
+            List<int> a = [], b = [];
+            int c = 0;
+            foreach (string line in input.Split('\n'))
+            {
+                string[] s = line.Split("   ");
+                a.Add(int.Parse(s[0]));
+                b.Add(int.Parse(s[1]));
+            }
+
+            a = a.Distinct().ToList();
+            b.Sort();
+
+            foreach (int n in a)
+            {
+                c += n * b.Count(num => num == n);
+            }
+
+            return c;
+        }
+    }
+}
