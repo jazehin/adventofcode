82783534a80ee16a8c476f92cd6200f093e4e463
jazehin <jazehin@outlook.com>
2024-12-02 22:01:09 +0100

    day 1 & day 2

--- Program.cs
+++ Program.cs
@@ -1,40 +1,53 @@
-﻿using adventofcode.y2024;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace adventofcode
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            //exit if AoC is not active (date is not between Dec 1 - Dec 25)
-            if (DateTime.Today.Month != 12 || DateTime.Now.Day > 25)
+            int year = DateTime.Now.Year;
+            int day = DateTime.Now.Day;
+
+            if (args.Length > 0)
+            {
+                year = int.Parse(args[0]);
+                day = int.Parse(args[1]);
+            }
+
+            if (year < 2015 || year > DateTime.Now.Year || day < 1 || day > 25)
             {
                 Console.WriteLine("Advent of Code is inactive!");
                 Console.ReadLine();
                 Environment.Exit(0);
             }
 
-            //dynamically get the class, and then the method to call
-            Type? type = null;
-            MethodInfo? methodInfo = null;
-
-            type = Type.GetType($"adventofcode.y{DateTime.Now.Year}.Day{DateTime.Now.Day:00}_1");
-            if (type is not null)
-                methodInfo = type.GetMethod("GetResult");
-            if (methodInfo is not null)
-                Console.WriteLine($"Day {DateTime.Now.Day:00}, part 1: {methodInfo.Invoke(null, null)}");
-
-            type = null;
-            methodInfo = null;
-
-            type = Type.GetType($"adventofcode.y{DateTime.Now.Year}.Day{DateTime.Now.Day:00}_2");
-            if (type is not null)
-                methodInfo = type.GetMethod("GetResult");
-            if (methodInfo is not null)
-                Console.WriteLine($"Day {DateTime.Now.Day:00}, part 2: {methodInfo.Invoke(null, null)}");
+            PrintResults(year, day);
 
             Console.ReadLine();
+        }
+
+        public static void PrintResults(int year, int day)
+        {
+            //dynamically get the class, and then the method to call
+            Type? type = Type.GetType($"adventofcode.y{year}.Day{day:00}");
+            MethodInfo? methodInfo = null;
+
+            //first problem of the day
+            if (type is not null)
+                methodInfo = type.GetMethod("GetResult1");
+            if (methodInfo is not null)
+                Console.WriteLine($"{year} December {day}, part 1: {methodInfo.Invoke(null, null)}");
+            else
+                Console.WriteLine($"{year} December {day}, part 1: #Problem not solved#");
+
+            //second problem of the day
+            if (type is not null)
+                methodInfo = type.GetMethod("GetResult2");
+            if (methodInfo is not null)
+                Console.WriteLine($"{year} December {day}, part 2: {methodInfo.Invoke(null, null)}");
+            else
+                Console.WriteLine($"{year} December {day}, part 2: #Problem not solved#");
         }
     }
 }
---
+++ y2024/Day02.cs
@@ -0,0 +1,70 @@
+﻿namespace adventofcode.y2024
+{
+    public class Day02
+    {
+        public static int GetResult1()
+        {
+            string input = File.ReadAllText("./y2024/res/02.txt");
+            int c = 0;
+
+            foreach (string line in input.Split("\n"))
+            {
+                string[] d = line.Split(' ');
+                List<int> nums = [];
+                foreach (string n in d) nums.Add(int.Parse(n));
+                if (IsLevelValid(nums)) c++;
+            }
+
+            return c;
+        }
+
+        private static bool IsLevelValid(List<int> nums)
+        {
+            if (nums.Count < 2) return true;
+            if (nums[0] < nums[1])
+            {
+                int i = 0;
+                while (i < nums.Count - 1 && nums[i] < nums[i + 1] && Math.Abs(nums[i] - nums[i + 1]) <= 3) i++;
+                return i == nums.Count - 1;
+            }
+            else
+            {
+                int i = 0;
+                while (i < nums.Count - 1 && nums[i] > nums[i + 1] && Math.Abs(nums[i] - nums[i + 1]) <= 3) i++;
+                return i == nums.Count - 1;
+            }
+        }
+
+        public static int GetResult2()
+        {
+            string input = File.ReadAllText("./y2024/res/02.txt");
+            int c = 0;
+
+            foreach (string line in input.Split("\n"))
+            {
+                string[] d = line.Split(' ');
+                List<int> nums = [];
+
+                foreach (string n in d)
+                    nums.Add(int.Parse(n));
+
+                if (IsLevelValid2(nums)) c++;
+            }
+
+            return c;
+        }
+
+        private static bool IsLevelValid2(List<int> nums)
+        {
+            for (int i = 0; i < nums.Count; i++)
+            {
+                List<int> list = [];
+                for (int j = 0; j < nums.Count; j++)
+                    if (i != j)
+                        list.Add(nums[j]);
+                if (IsLevelValid(list)) return true;
+            }
+            return false;
+        }
+    }
+}
